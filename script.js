ef6395ebc02a29b29ad0f2e7b7cae93b137a581f
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-13 19:52:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstChar(text) {
   // your code here
+	return text.length==0?'':text.charAt(0);
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
+const text = 
+prompt("Enter text:");
 alert(firstChar(text));
